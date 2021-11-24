25a397cd9a5a0fc9185fce046c95ec93af9f7ba5
ClementMoreauEPSI <aclemoro@free.fr>
2021-11-24 15:38:49 +0100

    Squashed commit of the following:
    commit bec40f2523794cea1cdb560edc7aac68857c31e2
    Author: ClementMoreauEPSI <aclemoro@free.fr>
    Date:   Wed Nov 24 15:36:57 2021 +0100
    
        Ajout HelperConfig
    
        Ajout helperConfig
        Ajout méthode création requete SQL a la volée
    
    commit 98859de13c3724ee9905feb9566fcffb5764dcf8
    Author: ClementMoreauEPSI <aclemoro@free.fr>
    Date:   Tue Nov 23 14:57:47 2021 +0100
    
        Add AES, DES
    
        Ajout méthode AES et DES
        Ajout méthode choix aléatoire méthodes  chiffrement
        Ajout méthode de chiffrage avec les méthodes aléatoires
        Ajout méthode de déchiffrage avec les méthodes aléatoires
    
    commit 5362fa66875327eba782124554e57618d676dcca
    Author: ClementMoreauEPSI <aclemoro@free.fr>
    Date:   Tue Nov 23 11:38:07 2021 +0100
    
        Add MD5
    
        Ajout méthode hash et déhash MD5
        Ajout fichier gitignore

---
+++ ProjetWorkshop/HelperConfig.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ProjetWorkshop
+{
+    class HelperConfig
+    {
+
+        private string CHEMIN_FICHIER_JSON = string.Concat(Directory.GetCurrentDirectory(), "/JSON/","Configuration.json");
+
+        public Dictionary<string, List<string>> DicoTable = new Dictionary<string, List<string>>();
+        public HelperConfig()
+        {
+            ChargerDictionnaireTable();
+            foreach(KeyValuePair<string, List<string>> table in DicoTable)
+            {
+                
+
+                StringBuilder sb = new StringBuilder();
+                sb.Append("UPDATE ");
+                sb.Append(table.Key);
+                sb.Append(" SET ");
+                foreach (string colonne in table.Value)
+                {
+                    sb.Append($" {colonne} = @aeDep , ");
+                }
+                sb.Remove(sb.Length - 1, 1);
+                sb.Append(" WHERE id = @id");
+
+                string test = sb.ToString();
+            }
+
+        }
+
+        private void ChargerDictionnaireTable()
+        {
+            string json = File.ReadAllText(CHEMIN_FICHIER_JSON);
+            try
+            {
+                if (!string.IsNullOrEmpty(json))
+                {
+                    JArray tables = JArray.Parse(json);
+                    foreach (JObject table in tables)
+                    {
+                        List<string> listColonnes = new List<string>();
+                        if (table["table"] != null && table["Colonnes"] != null)
+                        {
+                            JArray colonnes = JArray.Parse(table["Colonnes"].ToString());
+                            foreach (JObject colonne in colonnes)
+                            {
+                                if (colonne["Nom"] != null)
+                                {
+                                    listColonnes.Add(colonne["Nom"].ToString());
+                                }
+                            }
+                            DicoTable.Add(table["table"].ToString(), listColonnes);
+
+                        }
+                    }
+
+                }
+            }
+            catch(Exception exc)
+            { throw new Exception($"Erreur lors de la lecture du fichier de configuration avec l'erreur suivant : '{exc.Message}'"); }
+            
+        }
+    }
+}
--- ProjetWorkshop/Program.cs
+++ ProjetWorkshop/Program.cs
@@ -6,7 +6,63 @@ namespace ProjetWorkshop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HelperConfig helper = new HelperConfig();
+            string[] test = HelperHash.AleatoireChiffrement();
+            string message = "First, you should verify whether the input string is valid. Then, you need to define an instance of the";
+            string final = EncryptData(message, test);
+            string finalDecrypt = DecryptData(final, test);
+
         }
+
+        
+
+        private static string EncryptData(string input, string[] chiffrements)
+        {
+            string output = input;
+            foreach(string chiffrement in chiffrements)
+            {
+                switch(chiffrement)
+                {
+                    case "AES":
+                        output = HelperHash.CryptAES(output);
+                        break;
+                    case "MD5":
+                        output = HelperHash.CryptMD5(output);
+                        break;
+                    case "DES":
+                        output = HelperHash.CryptDES(output);
+                        break;
+                }
+            }
+
+            return output;
+        }
+
+        private static string DecryptData(string input, string[] chiffrements)
+        {
+            string output = input;
+
+            for(int i = chiffrements.Length-1; (i < chiffrements.Length && i >=0 ); i--)
+            {
+                switch(chiffrements[i])
+                {
+                    case "AES":
+                        output = HelperHash.DecryptAES(output);
+                        break;
+                    case "MD5":
+                        output = HelperHash.DeCryptMD5(output);
+                        break;
+                    case "DES":
+                        output = HelperHash.DeCryptDES(output);
+                        break;
+                }
+            }
+
+            return output;
+        }
+
+
+
+
     }
 }
